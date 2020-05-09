7f24bdf24bebbebd2e743531416cb4fad7750e2a
Ubandi77 <60453312+Ubandi77@users.noreply.github.com>
2020-05-09 13:43:08 -0400

    Add files via upload
    Solution for Powershell task.

---
+++ Task.ps1
@@ -0,0 +1,42 @@
+$name = Read-Host -Prompt 'Enter Phrase'
+
+
+
+$CharArray =$name.ToCharArray()
+
+
+
+$latestarr = @()
+
+
+
+For ($i=0; $i -lt $CharArray.Count ; $i++)
+ {
+  
+   
+
+ if($latestarr  -Contains $CharArray[$i]){
+   
+           
+          
+       Write-Host "Character the recurs first is"
+ 
+   
+       Write-Host $CharArray[$i]
+
+
+       break
+ 
+       
+     
+   } 
+ 
+   else{
+     
+          $latestarr  += $CharArray[$i]
+
+  
+      }    
+ 
+ 
+    }
